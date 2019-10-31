9d578fbb163a4a0514275e598f61d58edaf6b9dd
jbride <jbride2001@yahoo.com>
2019-10-31 02:02:21 -0600

    bin/setup_jenkins.sh

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -15,25 +15,57 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 # Set up Jenkins with sufficient resources
 # DO NOT FORGET TO PASS '-n ${GUID}-jenkins to ALL commands!!'
 # You do not want to set up things in the wrong project.
-# TBD
+oc new-app jenkins-persistent \
+   -p ENABLE_OAUTH=true \
+   -p MEMORY_LIMIT=4Gi \
+   -p VOLUME_CAPACITY=8Gi \
+   -p DISABLE_ADMINISTRATIVE_MONITORS=true \
+   -n $GUID-jenkins
 
 # Create custom agent container image with skopeo.
 # Build config must be called 'jenkins-agent-appdev' for the test below to succeed
-  # TBD
+oc new-build --strategy=docker -D \
+   $'FROM quay.io/openshift/origin-jenkins-agent-maven:4.1.0\n
+   USER root\n
+   RUN curl https://copr.fedorainfracloud.org/coprs/alsadi/dumb-init/repo/epel-7/alsadi-dumb-init-epel-7.repo -o /etc/yum.repos.d/alsadi-dumb-init-epel-7.repo && \ \n
+   curl https://raw.githubusercontent.com/cloudrouter/centos-repo/master/CentOS-Base.repo -o /etc/yum.repos.d/CentOS-Base.repo && \ \n
+   curl http://mirror.centos.org/centos-7/7/os/x86_64/RPM-GPG-KEY-CentOS-7 -o /etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7 && \ \n
+   DISABLES="--disablerepo=rhel-server-extras --disablerepo=rhel-server --disablerepo=rhel-fast-datapath --disablerepo=rhel-server-optional --disablerepo=rhel-server-ose --disablerepo=rhel-server-rhscl" && \ \n
+   yum $DISABLES -y --setopt=tsflags=nodocs install skopeo && yum clean all\n
+   USER 1001' \
+   --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 
 # Create Secret with credentials to access the private repository
 # You may hardcode your user id and password here because
 # this shell scripts lives in a private repository
 # Passing it from Jenkins would show it in the Jenkins Log
-# TBD
+oc secrets new-basicauth gogs-secret --username=jbride-redhat.com --password=ratwater -n ${GUID}-jenkins
 
 
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
 # Build config has to be called 'tasks-pipeline'.
 # Make sure you use your secret to access the repository
-# TBD
+echo "apiVersion: v1
+items:
+- kind: "BuildConfig"
+  apiVersion: "v1"
+  metadata:
+    name: "tasks-pipeline"
+  spec:
+    source:
+      type: "Git"
+      git:
+        uri: "https://homework-gitea.apps.shared.na.openshift.opentlc.com/jbride-redhat.com/ocp4_app_deploy_homework_template.git"
+    strategy:
+      type: "JenkinsPipeline"
+      jenkinsPipelineStrategy:
+        jenkinsfilePath: Jenkinsfile
+kind: List
+metadata: []" | oc apply -f - -n ${GUID}-jenkins
+
+oc set build-secret --source bc/tasks-pipeline gogs-secret -n ${GUID}-jenkins
 
 
 
